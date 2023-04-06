c6b1e62e3282c88657ae5e04bb33b8f9b8bbda33
goodboyx <goodboyx@naver.com>
2023-04-06 14:57:26 +0900

    일반 provider 변경

--- lib/provider/stepProvider.dart
+++ lib/provider/stepProvider.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class StepProvider extends ChangeNotifier {
+  static final StepProvider _singleton = StepProvider._internal();
+
+  factory StepProvider() { return _singleton; }
+
+  StepProvider._internal();
+
   int step = 0;
 
   getStep() => step;
 
   setStep(int step2) {
-    if (step == step2) {
-      notifyListeners();
-    }
     step = step2;
+    notifyListeners();
   }
 }
